282478287093ff1a78454be5146462131786ce95
yashsingh8624 <yashsingh8624@gmail.com>
2025-12-15 21:28:57 +0530

    Update google script.js

--- google script.js
+++ google script.js
@@ -1,46 +1,36 @@
-fetch("https://opensheet.elk.sh/YOUR_SHEET_ID/Sheet1")
-.then(res => res.json())
-.then(data => {
-  let box = document.getElementById("products");
-  data.forEach(item => {
-    box.innerHTML += `
-      <div class="card">
-        <img src="${item.image}" />
-        <h3>${item.name}</h3>
-        <p>₹ ${item.price}</p>
-      </div>`;
-  });
-});
 let cart = [];
 
 fetch("https://opensheet.elk.sh/YOUR_SHEET_ID/Sheet1")
-.then(res => res.json())
-.then(data => {
-  let box = document.getElementById("products");
-  data.forEach((item, index) => {
-    box.innerHTML += `
-      <div class="card">
-        <img src="${item.image}" />
-        <h3>${item.name}</h3>
-        <p>₹ ${item.price}</p>
-        <button onclick="addToCart(${index})">Add to Cart</button>
-      </div>`;
+  .then(response => response.json())
+  .then(data => {
+    const productsDiv = document.getElementById("products");
+
+    data.forEach((item, index) => {
+      productsDiv.innerHTML += `
+        <div class="product-card">
+          <img src="${item.image}" alt="${item.name}">
+          <h3>${item.name}</h3>
+          <p>₹${item.price}</p>
+          <button onclick="addToCart(${index})">Add to Cart</button>
+        </div>
+      `;
+    });
+
+    window.products = data;
   });
 
-  window.products = data;
-});
-
-function addToCart(i) {
-  cart.push(products[i]);
+function addToCart(index) {
+  cart.push(window.products[index]);
   document.getElementById("cartCount").innerText = cart.length;
 }
 
 function orderWhatsApp() {
-  let msg = "New Order%0A";
-  cart.forEach(p => {
-    msg += `${p.name} - ₹${p.price}%0A`;
+  let message = "New Order:%0A";
+
+  cart.forEach(item => {
+    message += `${item.name} - ₹${item.price}%0A`;
   });
 
   let phone = "918624091826"; // apna number
-  window.open(`https://wa.me/${phone}?text=${msg}`);
+  window.open(`https://wa.me/${phone}?text=${message}`);
 }
